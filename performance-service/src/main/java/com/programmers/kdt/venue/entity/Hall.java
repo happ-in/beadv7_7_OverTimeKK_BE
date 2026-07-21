6825414ccb700444ccf73fb6275947bb1b205994
Hyebin Park <36289638+happ-in@users.noreply.github.com>
2026-07-21 17:34:39 +0900

    refactor: 엔티티 매핑 명시화 및 공연장 더미 데이터 추가 (#39)
    * refactor: 엔티티 매핑 명시화
    
    * chore: Venue, Hall, Seat 초기 데이터 적재
    
    * refactor: exception 패키지 구조 통합
    
    * refactor: 와일드카드 import를 명시적 import로 변경

--- performance-service/src/main/java/com/programmers/kdt/venue/entity/Hall.java
+++ performance-service/src/main/java/com/programmers/kdt/venue/entity/Hall.java
@@ -7,8 +7,10 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -18,19 +20,22 @@ import java.util.List;
 @Entity
 @Getter
 @NoArgsConstructor
+@Table(name = "hall")
 public class Hall extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "hall_id")
     private Long hallId;
 
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "venue_id", nullable = false)
     private Venue venue;
 
-    @Column(nullable = false)
+    @Column(name = "hall_name", nullable = false)
     private String hallName;
 
-    @Column(nullable = false)
+    @Column(name = "seat_total_count", nullable = false)
     private Long seatTotalCount;
 
     @OneToMany(mappedBy = "hall", fetch = FetchType.LAZY)
--- performance-service/src/main/java/com/programmers/kdt/venue/entity/Seat.java
+++ performance-service/src/main/java/com/programmers/kdt/venue/entity/Seat.java
@@ -7,26 +7,33 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
 @NoArgsConstructor
+@Table(name = "seat")
 public class Seat extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "seat_id")
     private Long seatId;
 
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "hall_id")
     private Hall hall;
 
-    @Column(nullable = false)
+    @Column(name = "zone", nullable = false)
     private String zone;
 
+    @Column(name = "seat_row")
     private String seatRow;
 
+    @Column(name = "seat_num")
     private String seatNum;
 }
