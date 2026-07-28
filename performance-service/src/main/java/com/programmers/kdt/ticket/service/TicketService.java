43015917fd93b4d7c50bbbe9cdfca9b6bd6f0f8d
happ-in <gpqls304@naver.com>
2026-07-28 23:06:40 +0900

    feat: 결제취소에 의한 티켓 점유 상태 변경

--- performance-service/src/main/java/com/programmers/kdt/ticket/service/TicketService.java
+++ performance-service/src/main/java/com/programmers/kdt/ticket/service/TicketService.java
@@ -5,11 +5,8 @@ import com.programmers.kdt.standby.event.StandbyTicketEvent;
 import com.programmers.kdt.ticket.dto.CheckTicketHoldAvailableRequest;
 import com.programmers.kdt.ticket.dto.CheckTicketHoldAvailableResponse;
 import com.programmers.kdt.ticket.dto.CreateStandbyResponse;
-import com.programmers.kdt.ticket.dto.OrderTicketResponse;
 import com.programmers.kdt.ticket.dto.ReleaseTicketHoldRequest;
 import com.programmers.kdt.ticket.dto.SessionStartDateResponse;
-
-import java.util.List;
 
 public interface TicketService {
     CreateStandbyResponse issueStandby(Long userId, Long sessionNum, String zone);
@@ -23,6 +20,4 @@ public interface TicketService {
     void standbyTicket(StandbyTicketEvent event);
 
     void changeTicketStatusByStandby(StandbyCheckResponseEvent event);
-
-    List<OrderTicketResponse> findOrderedTicketInfo(Long userId);
 }
