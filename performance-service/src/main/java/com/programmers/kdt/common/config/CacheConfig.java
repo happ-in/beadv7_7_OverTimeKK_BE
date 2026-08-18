4c3b90f3c975e9b5f24afd20c10e72fca59f8436
happ-in <gpqls304@naver.com>
2026-08-18 14:09:06 +0900

    refactor: 공연조회 캐시적용

--- performance-service/src/main/java/com/programmers/kdt/common/config/CacheConfig.java
+++ performance-service/src/main/java/com/programmers/kdt/common/config/CacheConfig.java
@@ -4,7 +4,7 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.GenericJacksonJsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 
 import java.time.Duration;
@@ -15,10 +15,14 @@ public class CacheConfig {
 
     @Bean
     public RedisCacheConfiguration cacheConfiguration() {
+        GenericJacksonJsonRedisSerializer serializer = GenericJacksonJsonRedisSerializer.builder()
+                .enableUnsafeDefaultTyping()
+                .build();
+
         return RedisCacheConfiguration.defaultCacheConfig()
-                .entryTtl(Duration.ofMinutes(30)) // TTL 30분 설정
+                .entryTtl(Duration.ofMinutes(30))
                 .serializeValuesWith(
-                        RedisSerializationContext.SerializationPair
-                                .fromSerializer(new GenericJackson2JsonRedisSerializer()));
+                        RedisSerializationContext.SerializationPair.fromSerializer(serializer)
+                );
     }
 }
