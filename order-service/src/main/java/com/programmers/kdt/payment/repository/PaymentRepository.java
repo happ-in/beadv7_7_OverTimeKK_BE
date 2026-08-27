5f7d3519a06d397b70a9af7c622e11ddbab7ceac
Yuhyun <rladbgus781@gmail.com>
2026-08-27 09:29:36 +0900

    refactor: 결제 승인 PG 트랜잭션 분리 및 미확인 결제 재조회 배치 추가
    * feat: 결제 승인 PG 트랜잭션 분리
    
    * feat: PG 승인 응답 없는 경우 결제 재조회 배치 추가
    
    * fix: 결제 완료/실패 이벤트가 트랜잭션 없을때 드롭되던 현상 해결 및 재조회 배치 테스트 작성
    
    * fix: 결제 승인 tx1/tx2 정합성 갭 해소 및 재조회 배치 age 필터 추가
    
    * fix: PaymentService 깨지는 테스트들 수정
    
    * feat: 멱등키 테스트코드 작성
    
    * feat: 트랜잭션 분리 테스트작성
    
    * refactor: Mock 객체에 지연시간 적용

--- order-service/src/main/java/com/programmers/kdt/payment/repository/PaymentRepository.java
+++ order-service/src/main/java/com/programmers/kdt/payment/repository/PaymentRepository.java
@@ -1,10 +1,12 @@
 package com.programmers.kdt.payment.repository;
 
 import com.programmers.kdt.payment.entity.Payment;
+import com.programmers.kdt.payment.entity.PaymentStatus;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 
@@ -14,4 +16,6 @@ public interface PaymentRepository extends JpaRepository<Payment, Long> {
     boolean existsByOrderId(Long orderId);
 
     Optional<Payment> findByOrderId(Long orderId);
+
+    Page<Payment> findByPaymentStatusAndModifiedAtBefore(PaymentStatus paymentStatus, LocalDateTime cutoffTime, Pageable pageable);
 }
